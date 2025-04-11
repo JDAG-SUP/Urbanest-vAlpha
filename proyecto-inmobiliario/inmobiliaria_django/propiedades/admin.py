78bd0446931f1a734055c9f855bae5621dddb590
Juan David Acevedo <133882599+JDAG-SUP@users.noreply.github.com>
2025-04-11 09:29:58 -0500

    Delete proyecto-inmobiliario directory

--- proyecto-inmobiliario/inmobiliaria_django/propiedades/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Propiedad
-
-@admin.register(Propiedad)
-class PropiedadAdmin(admin.ModelAdmin):
-    list_display = ('id', 'distrito', 'metros_cuadrados', 'habitaciones', 'precio', 'antiguedad', 'estado')
-    list_filter = ('distrito', 'habitaciones', 'estado')
-    search_fields = ('distrito', 'estado')
-    ordering = ('precio',) 
